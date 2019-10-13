537487fbe7da694cd837209a1af1f103d9506764
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 14:28:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,11 @@ def oxford_comma(array)
   # if statement to check how many elements are in the array
   if array.length == 1
     # .join converts an array into a string
-    array.join
+    array.join ( " ")
   elsif array.length == 2 
+    # .join(' and ') adds an and between each element
     array.join(' and ')
+    
   elsif array.length == 3
     return "#{array[0...-1].join(", ")}, and #{array.last}" #array[0...-1].join
   else array.length > 3
